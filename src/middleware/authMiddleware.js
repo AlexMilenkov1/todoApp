0c397f39e1db0ad7c5231c3b981857869f044456
Alex Milenkov <alexmilenkov07@gmail.com>
2025-08-10 22:38:39 +0300

    made authentication and middleware for JWT token

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken"
+
+function authMiddleware(req, res, next) {
+    const token = req.headers['Authorization']
+
+    if (token) {return res.status(401).json({message: "Missing JWT token!"})}
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({message: "Invalid JWT token!"})
+        }
+
+        req.userId = decoded.id
+        next()
+    })
+}
+
+export default authMiddleware
---
+++ src/routes/authRoutes.js
@@ -0,0 +1,75 @@
+import express from 'express'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import db from '../db.js'
+
+const router = express.Router();
+
+router.post('/register', (req, res) => {
+    const { username, password } = req.body
+
+    const hashedPassword = bcrypt.hashSync(password, 8)
+
+    try {
+
+        const insertUser = db.prepare(`
+            INSERT INTO users (username, password) VALUES (?, ?)
+        `)
+
+        const result = insertUser.run(username, hashedPassword)
+
+        const defaultTodo = 'Hello, here you can add your first todo!'
+        const insertTodo = db.prepare(`
+            INSERT INTO todos (user_id, task) VALUES (?, ?)
+        `)
+
+        const lastUserId = result.lastInsertRowid
+
+        insertTodo.run(lastUserId, defaultTodo)
+
+        const token = jwt.sign({ id: lastUserId }, process.env.JWT_SECRET, { expiresIn: '24h' })
+
+        res.json({ token })
+
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+
+
+})
+
+router.post('/login', (req, res) => {
+    const { username, password } = req.body
+
+    try {
+        // Search for user in the database
+        const findUser = db.prepare(`
+        SELECT * FROM users WHERE username = ?
+        `)
+
+        const user = findUser.get(username)
+
+        if (!user) {
+            return res.status(400).send({message: 'User not found!'})
+        }
+
+        const isPasswordValid = bcrypt.compareSync(password, user.password)
+
+        if (!isPasswordValid) {
+            return res.status(401).send({message: 'Please enter a valid passwod!'})
+        }
+
+        console.log(user);
+        
+        const token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: '24h'})
+        res.json({token})
+
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500)
+    }
+
+})
+
+export default router
